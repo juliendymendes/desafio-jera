9dab1f122f8b7773878fa2b03b083524e0056b70
Juliendy Mendes <juliendy0708@gmail.com>
2024-06-02 12:49:25 -0400

    feat: componente de navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Navbar from './components/NavBar.vue'
 </script>
 
 <template>
+  <Navbar />
   <RouterView />
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav className="navbar bg-base-100">
+        <div className="flex-1">
+            <router-link to="/suggested_movies" className="btn btn-ghost text-xl">Movies</router-link>
+        </div>
+        <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+                <li><router-link to="/watchlist">Minha lista</router-link></li>
+            </ul>
+        </div>
+    </nav>
+</template>
